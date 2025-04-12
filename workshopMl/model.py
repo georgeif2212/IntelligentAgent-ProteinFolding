de10817f73f887f759172992b9f44b7a13083f77
Jorge Infante <jinfante2212@gmail.com>
2025-04-11 20:49:36 -0500

    feat: new code to exchange string values into numeric values

--- workshopMl/model.py
+++ workshopMl/model.py
@@ -1,14 +1,14 @@
 import pandas as pd
-# import numpy as np
-# import matplotlib.pyplot as plt
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.preprocessing import LabelEncoder
 
 # * load dataset
 df = pd.read_csv("ObesityDataSet.csv")
 
-# * show first rows
 print("Dataset dimensions:", df.shape)
-print("\First 5rows:")
+print("\nFirst 5 rows:")
 print(df.head())
 
 # * show data types
@@ -20,3 +20,19 @@ print("\nUnique values per categoric variable:")
 for col in df.columns:
     if df[col].dtype == "object":
         print(f"{col}: {df[col].unique()}")
+
+
+# * Create copy of dataset to change categorical columns string into int
+data = df.copy()
+
+# * Identify categorical columns
+categorical_cols = data.select_dtypes(include='object').columns.tolist()
+print("\nCategorical columns:", categorical_cols)
+label_encoders = {}
+
+# * Apply LabelEncoder to each categorical column
+for col in categorical_cols:
+    le = LabelEncoder()
+    data[col] = le.fit_transform(data[col])
+    label_encoders[col] = le
+
